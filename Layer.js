65e431087919dff0281782ee77377b0e4906ed09
Bruce Cooner <brucecooner@gmail.com>
2019-01-07 00:36:40 -0700

    added fill color button (stub only), initial Layer class

--- Layer.js
+++ Layer.js
@@ -1,15 +1,34 @@
 /**
- * pipe:
+ * pipeline:
  *    line points generator -> spoke points generator?
+ * 		seems unnecessary? Can generate spoke points once then clone upon rendering, letting svg transform handle
+ * 		the rotation
  */
-function LayerInit()
+function FractalaLayerInit()
 {
 	class Layer
 	{
+		get layer_default_parameters() 
+		{ return {	number_of_spokes:6, 
+						spoke_length:50,
+						spoke_rot_offset:0,
+						opacity:1,
+						radius:20,	// todo: layer types (this is circles specific)
+						stroke_width:1,
+						stroke:"hsl(0,100%,50%)",	// default red
+						fill:"",	// "" == transparent
+			}; 
+		};
+
 		get className()	{ return "Layer"; };
 
-		constructor()
+		// ------------------------------------------------------------
+		constructor(parameters)
 		{
+			Object.assign(layer, this.layer_default_parameters);
+			Object.assign(layer, parameters);
 		}
 	};
+
+	return { Layer:Layer };
 };
